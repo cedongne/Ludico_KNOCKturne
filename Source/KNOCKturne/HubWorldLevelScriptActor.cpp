c24f04eaab89f1b549e2e4dc0e60554b1f851f6e
cedongne <cedongne@gmail.com>
2022-11-14 13:12:25 +0900

    Code modifying

--- Source/KNOCKturne/HubWorldLevelScriptActor.cpp
+++ Source/KNOCKturne/HubWorldLevelScriptActor.cpp
@@ -5,10 +5,14 @@
 
 AHubWorldLevelScriptActor::AHubWorldLevelScriptActor() {
 	NTLOG(Warning, TEXT("Hubworld Level Script"));
+//	DialogueTableComponent = CreateDefaultSubobject<UDialogueTableComponent>(TEXT("DialogueManager"));
+//	DialogueTableComponent->LoadDialogueTable("/Game/Assets/DataTable/Dialogue_Prologue.Dialogue_Prologue");
+}
+
+void AHubWorldLevelScriptActor::BeginPlay() {
+	Super::BeginPlay();
+
+	NTLOG_S(Warning);
 	DialogueTableComponent = CreateDefaultSubobject<UDialogueTableComponent>(TEXT("DialogueManager"));
 	DialogueTableComponent->LoadDialogueTable("/Game/Assets/DataTable/Dialogue_Prologue.Dialogue_Prologue");
-
-	auto DataRow = DialogueTableComponent->GetData("Prologue_2");
-	NTLOG(Warning, TEXT("%s"), *(DataRow->CharacterStringID));
-//	DialogueManager = CreateDefaultSubobject<UDialogueManager>(TEXT("DialogueManager"));
 }
