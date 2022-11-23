086c73b269be57f0c14e7bbde5864ff942b9d6aa
YoungMin Song <cedongne@gmail.com>
2022-11-23 14:13:14 +0900

    SetHubworldModel

--- Source/KNOCKturne/DialogueTableComponent.cpp
+++ Source/KNOCKturne/DialogueTableComponent.cpp
@@ -20,9 +20,44 @@ UDialogueTableComponent::UDialogueTableComponent(FString TablePath)
 void UDialogueTableComponent::LoadDialogueTable(FString TablePath) {
 	static ConstructorHelpers::FObjectFinder<UDataTable> DT_TABLE(*TablePath);
 	DialogueTable = DT_TABLE.Object;
+<<<<<<< Updated upstream
+=======
+	NTCHECK(DialogueTable == nullptr);
+	NTLOG_S(Warning);
+	DialogueTable->GetAllRows<FDialogueData>("GetAllRows", DialogueRows);
+	NTCHECK(DialogueRows.IsValidIndex(0));
+	NTLOG_S(Warning);
+>>>>>>> Stashed changes
 }
 
 FDialogueData* UDialogueTableComponent::GetData(FString RowID) {
 	return DialogueTable->FindRow<FDialogueData>(*RowID, TEXT(""));
 }
 
+<<<<<<< Updated upstream
+=======
+FString UDialogueTableComponent::GetString(FDialogueData* DataRow) {
+	return StringTable->FindRow<FDialogueString>(*(DataRow->StringID), TEXT(""))->KOR;
+}
+
+FString UDialogueTableComponent::GetStringOnBP(FDialogueData DataRow) {
+	NTLOG(Warning, TEXT("%d"), CurrentRow);
+	return StringTable->FindRow<FDialogueString>(*(DataRow.StringID), TEXT(""))->KOR;
+}
+
+int32 UDialogueTableComponent::GetDialogueTableRowSize() {
+	return DialogueTable->GetRowMap().Num();
+}
+
+FDialogueData UDialogueTableComponent::GetNextRowDialogueTable() {
+	if (++CurrentRow == DialogueRows.Num() - 1) {
+		IsEndedDialogueRows = true;
+	}
+	return *(DialogueRows[CurrentRow]);
+}
+
+void UDialogueTableComponent::ResetDialogueRowPointer() {
+	IsEndedDialogueRows = false;
+	CurrentRow = 0;
+}
+>>>>>>> Stashed changes
--- Source/KNOCKturne/NPC.cpp
+++ Source/KNOCKturne/NPC.cpp
@@ -6,27 +6,30 @@
 // Sets default values
 ANPC::ANPC()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	/*
+	InteractionPopupUI = CreateDefaultSubobject<UWidgetComponent>(TEXT("InteractionWidget"));
+	InteractionPopupUI->SetupAttachment(RootComponent);
+
+	InteractionPopupUI->SetRelativeLocation(FVector(0.0f, 0.0f, 180.0f));
+	InteractionPopupUI->SetWidgetSpace(EWidgetSpace::Screen);
+
+	static ConstructorHelpers::FClassFinder<UUserWidget> INTERACTIONPOPUP_UI(TEXT("/Game/Blueprints/UMG/InteractionPopup.InteractionPopup"));
+	if (INTERACTIONPOPUP_UI.Succeeded()) {
+		InteractionPopupUI->SetWidgetClass(INTERACTIONPOPUP_UI.Class);
+		InteractionPopupUI->SetDrawSize(FVector2D(150.0f, 50.0f));
+	}
+	*/
 
 	IsHoverMouse = false;
-
 }
 
 // Called when the game starts or when spawned
 void ANPC::BeginPlay()
 {
-	
 	Super::BeginPlay();
-	
+//	SetInteractionPopupWidget(InteractionPopupWidget);
 }
 
-// Called every frame
-void ANPC::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 // Called to bind functionality to input
 void ANPC::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -35,3 +38,10 @@ void ANPC::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void ANPC::SetCanInteraction_Implementation() {
+
+}
+
+void ANPC::SetCannotInteraction_Implementation() {
+
+}
