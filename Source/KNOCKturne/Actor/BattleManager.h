8badb25a584a150879316c036c2750508dce8653
YoungMin Song <cedongne@gmail.com>
2023-09-03 19:25:30 +0900

    Fix useless gitignore
    불필요한 ignore를 제거합니다.

---
+++ Source/KNOCKturne/Actor/BattleManager.h
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../KNOCKturne.h"
+#include "GameFramework/Actor.h"
+
+#include "Boss.h"
+#include "Peppy.h"
+
+#include "BattleManager.generated.h"
+
+enum BattleTurnType {
+	BossTurn,
+	PeppySkillSelectingTurn,
+	PeppySkillUsingTurn
+};
+
+UCLASS()
+class KNOCKTURNE_API ABattleManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ABattleManager();
+
+public:
+	UWorld* world;
+	FTimerHandle TurnOverTimer;
+	FTimerManager LeftTurnTimeTimerManager;
+	FTimerHandle LeftTurnTimeTimer;
+
+	BattleTurnType CurrentTurnType;
+	float LeftCurrentTurnTime;
+
+	bool IsCalled_InitStartBossTurn = false;
+
+	void Init();
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_ActorInit();
+
+	void StartBossTurn();
+	void StartPeppyTurn();
+
+	void TurnChange();
+
+	void RunTurnTimer(float DeltaTime);
+	void DecreaseLeftCurrentTurnTime();
+
+	void ProcessDamageBeforeStartTurn();
+
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> BossActorSubClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TMap<FString, AActor*> SkillActorsOnField;
+	
+	UFUNCTION(BlueprintCallable)
+	void BP_InitStartBattle();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_StartBossTurn();
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_StartPeppyTurn();
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_UsePeppySkills();
+
+	UFUNCTION(BlueprintCallable)
+	float GetLeftCurrentTurnTime();
+	UFUNCTION(BlueprintCallable)
+	void SetLeftCurrentTurnTime(float TurnTime);
+	UFUNCTION(BlueprintCallable)
+	void EndTurn();
+	UFUNCTION(BlueprintCallable)
+	void EndBattle();
+
+private:
+	class TArray<UBuffComponent*> HandledBuffComponents;
+	void HandleBuffsEachTurns();
+	void HandleBuffsEachSeconds();
+
+	FTimerHandle SecondBuffsHandler;
+
+protected:
+	class UBattleTableManagerSystem* BattleTableManagerSystem;
+	class UBattleManagerSystem* BattleManagerSystem;
+	class UActorManagerSystem* ActorManagerSystem;
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
