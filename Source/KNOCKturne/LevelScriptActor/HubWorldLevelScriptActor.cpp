0bf5ae9ba43d7ffcd399823cadb57814a4c5f71f
Chaeeun Lim <limce106@naver.com>
2023-09-08 00:10:41 +0900

    Add Functions in HubWorld BP
    - 허브월드 레벨 블루프린트 내 함수들을 HubWorldLevelScriptActor에 구현
    - StartPrologueDialogue 함수 내에서 BP_DreamM을 불러오지 못하고 있는 문제가 있음

--- Source/KNOCKturne/LevelScriptActor/HubWorldLevelScriptActor.cpp
+++ Source/KNOCKturne/LevelScriptActor/HubWorldLevelScriptActor.cpp
@@ -6,6 +6,10 @@
 
 AHubWorldLevelScriptActor::AHubWorldLevelScriptActor() {
 	PrologueDialogueComponent = CreateDefaultSubobject<UDialogueTableComponent>(TEXT("DialogueMananger"));
+	UGameInstance* GameInstance = Cast<UGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+	KNOCKturneGameState = Cast<AKNOCKturneGameState>(UGameplayStatics::GetGameState(GetWorld()));
+	PeppyController = (APeppyController*)UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	Peppy = Cast<APeppy>(UGameplayStatics::GetPlayerPawn(this, 0));
 }
 
 void AHubWorldLevelScriptActor::BeginPlay() {
@@ -14,3 +18,136 @@ void AHubWorldLevelScriptActor::BeginPlay() {
 	DialogueTableComponent->LoadDialogueTable("Dialogue_Npc");
 	PrologueDialogueComponent->LoadDialogueTable("Dialogue_Prologue");
 }
+
+//void AHubWorldLevelScriptActor::StartPrologueDialogue() {
+//	PeppyController->PrologueInProcess = true;
+//	PrologueDialogueComponent->ResetDialogueRowPointer();
+//
+//	// BP_Dialogue 위젯 생성
+//	if (DialogueWidgetClass) {
+//		DialogueWidgetRef = CreateWidget<UDialogueWidget>(GetWorld(), DialogueWidgetClass);
+//		if (DialogueWidgetRef) {
+//			DialogueWidgetRef->AddToViewport();
+//		}
+//	}
+//
+//	DialogueWidgetRef->TextBlock_E->SetVisibility(ESlateVisibility::Visible);
+//	
+//	// Get Actor of Class(BP_DreamM)
+//	// Set Actor Rotation (0.0, 0.0, -70.0)
+//
+//	DialogueWidgetRef->GetNextDialogueLine(PrologueDialogueComponent);
+//}
+
+bool AHubWorldLevelScriptActor::MoveDirectionTF(FDialogueData DataTable) {
+	if (DataTable.Direction.Contains("DT")) {
+		return true;
+	}
+	else if (DataTable.Direction.Contains("DF")) {
+		return false;
+	}
+	else {
+		return NULL;
+	}
+}
+
+//void AHubWorldLevelScriptActor::StorePrevCameraRotation() {
+//	PrevCameraRotation = Peppy->Camera->GetComponentRotation();
+//}
+//
+//void AHubWorldLevelScriptActor::StorePrevCameraLocation() {
+//	PrevCameraLocation = Peppy->Camera->GetComponentLocation();
+//}
+//
+//void AHubWorldLevelScriptActor::StorePrevCameraTransform() {
+//	StorePrevCameraLocation();
+//	StorePrevCameraRotation();
+//}
+//
+//void AHubWorldLevelScriptActor::PrologueDirection(FDialogueData DataTable) {
+//	if (DataTable.Direction.Contains("none")) {
+//		CallMoveDirection(DataTable);
+//	}
+//	else if (DataTable.Direction.Contains("before")) {
+//		BlackScreenOff();
+//		CallMoveDirection(DataTable);
+//	}
+//	else if (DataTable.Direction.Contains("after")) {
+//		CallMoveDirection(DataTable);
+//		BlackScreenOn();
+//	}
+//	else if (DataTable.Direction.Contains("both")) {
+//		BlackScreenOff();
+//		CallMoveDirection(DataTable);
+//		BlackScreenOn();
+//	}
+//}
+//
+//void AHubWorldLevelScriptActor::CallMoveDirection(FDialogueData DataTable) {
+//	bool isDirectionExist = MoveDirectionTF(DataTable);
+//
+//	if (isDirectionExist) {
+//		MoveDirection();
+//		MDSelection++;
+//	}
+//	else {
+//		DialogueWidgetRef->NextTalk(PrologueDialogueComponent);
+//		DialogueWidgetRef->SetVisibility(ESlateVisibility::Visible);
+//	}
+//}
+//
+//void AHubWorldLevelScriptActor::NextIsDirection(FDialogueData DataTable) {
+//	if (DataTable.Direction == "-1") {
+//		DialogueWidgetRef->isDirection = false;
+//	}
+//	else {
+//		DialogueWidgetRef->isDirection = true;
+//	}
+//}
+//
+//void AHubWorldLevelScriptActor::AfterPrologueDirection() {
+//	DialogueWidgetRef->isCameraMoving = false;
+//	DialogueWidgetRef->RichTextBlock_Dialogue->SetText(FText::FromString(""));
+//	DialogueWidgetRef->NextTalk(PrologueDialogueComponent);
+//	DialogueWidgetRef->SetVisibility(ESlateVisibility::Visible);
+//}
+//
+//void AHubWorldLevelScriptActor::SetPeppyHiddenOrNot() {
+//	if (isPrologue) {
+//		if (KNOCKturneGameState->isBattleFail) {
+//			Peppy->SetActorHiddenInGame(true);
+//		}
+//		else {
+//			Peppy->SetActorHiddenInGame(false);
+//		}
+//	}
+//	else {
+//		Peppy->SetActorHiddenInGame(true);
+//	}
+//}
+//
+//void AHubWorldLevelScriptActor::BattleFailDialogueAllEnded() {
+//	DialogueWidgetRef->RemoveFromParent();
+//
+//	if (KNOCKturneGameState->isDreamDiaryUpdated) {
+//		KNOCKturneGameState->isDreamDiaryUpdated = false;
+//	}
+//	if (KNOCKturneGameState->GetDreamFragment) {
+//		KNOCKturneGameState->GetDreamFragment = false;
+//	}
+//	Peppy->Camera->SetWorldTransform(OriginalCameraTransform);
+//	Peppy->SetActorHiddenInGame(false);
+//}
+//
+//void AHubWorldLevelScriptActor::CreateHubworldHUD() {
+//	if (HubworldHUDClass) {
+//		HubworldHUDRef = CreateWidget<UDialogueWidget>(GetWorld(), HubworldHUDClass);
+//		if (HubworldHUDRef) {
+//			HubworldHUDRef->AddToViewport();
+//		}
+//	}
+//
+//	if (KNOCKturneGameState->isDreamDiaryUpdated) {
+//		// HubworldHUDRef->Image_DreamDiary
+//	}
+//}
--- Source/KNOCKturne/LevelScriptActor/HubWorldLevelScriptActor.h
+++ Source/KNOCKturne/LevelScriptActor/HubWorldLevelScriptActor.h
@@ -6,6 +6,10 @@
 #include "Engine/LevelScriptActor.h"
 
 #include "KNOCKturneLevelScriptActor.h"
+#include "GameMode/KNOCKturneGameState.h"
+#include "GameMode/PeppyController.h"
+#include "Widget/DialogueWidget.h"
+#include "Actor/Peppy.h"
 
 #include "HubWorldLevelScriptActor.generated.h"
 
@@ -16,14 +20,77 @@ UCLASS()
 class KNOCKTURNE_API AHubWorldLevelScriptActor : public AKNOCKturneLevelScriptActor
 {
 	GENERATED_BODY()
+
+	AKNOCKturneGameState* KNOCKturneGameState;
 	
 public:
 	AHubWorldLevelScriptActor();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
 	UDialogueTableComponent* PrologueDialogueComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MDSelection = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool isPrologue = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator PrevCameraRotation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector PrevCameraLocation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator PrevPeppyRotation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector PrevPeppyLocation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform OriginalRabbitTransform;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator PrevRabbitRotation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform OriginalPeppyTransform;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTransform OriginalCameraTransform;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator PrevDreamMRotation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector OriginalDreamMLocation;
 
 protected:
 	virtual void BeginPlay() override;
 
+	APeppyController* PeppyController;
+	APeppy* Peppy;
+	TSubclassOf<UUserWidget> DialogueWidgetClass;
+	class UDialogueWidget* DialogueWidgetRef;
+	/*TSubclassOf<UUserWidget> HubworldHUDClass;
+	class UDialogueWidget* HubworldHUDRef;*/
+
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void BlackScreenOn();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void BlackScreenOff();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void MoveDirection();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void StartPrologueDialogue();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	bool MoveDirectionTF(FDialogueData DataTable);
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void StorePrevCameraRotation();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void StorePrevCameraLocation();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void StorePrevCameraTransform();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void PrologueDirection(FDialogueData DataTable);
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void CallMoveDirection(FDialogueData DataTable);
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void NextIsDirection(FDialogueData DataTable);
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void AfterPrologueDirection();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void SetPeppyHiddenOrNot();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void BattleFailDialogueAllEnded();
+	UFUNCTION(Blueprintcallable, BlueprintImplementableEvent)
+	void CreateHubworldHUD();
 };
